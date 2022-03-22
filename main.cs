aec342656e590195165258d129ec709af7932109
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-22 17:30:06 +0000

    12 files

--- main.cs
+++ main.cs
@@ -1,7 +1,27 @@
+// Created by: Marshall Demars
+// Created on: Mar 2022
+//
+// This program accepts user input
+
 using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+     // This function accepts user input
+        string streetName;
+        int streetNumber;
+      
+        Console.WriteLine ("This program gets a user's address");
+        Console.WriteLine("");
+
+        Console.Write("Enter your street name: ");
+        streetName = Console.ReadLine();
+        Console.Write("Enter your street number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("Your street address is: " + streetNumber + " " + streetName + ".");
+
+        Console.WriteLine("\nDone.");
   }
 }
